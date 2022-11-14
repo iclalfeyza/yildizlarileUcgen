56384bebe9204c990f4a50e885251995d45ae89e
iclal feyza <iclalfeyzaozeken@hotmail.com>
2022-11-14 21:18:07 +0300

    l

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner klavye = new Scanner(System.in);
-        System.out.println("Bir Sayı Giriniz :");
+        System.out.print("Bir Sayı Giriniz :");
         int n = klavye.nextInt();
 
-        for (int i = 0; i <= n ; i++) {
+        for (int i = 1; i <= n ; i++) {
             for (int j = 0; j < (n - i); j++) {
                 System.out.print(" ");
             }
-            for (int k = 1; k <= (2 * i + 1); k++) {
+            for (int k = 1; k <= (2 * i - 1); k++) {
                 System.out.print("*");
             }
             System.out.println(" ");
